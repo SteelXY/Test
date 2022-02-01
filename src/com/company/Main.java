06cde7ba9bbddff02fc2d6011ce2e9260850a13d
SteelXY <zoraks9755@gmail.com>
2022-02-02 01:03:18 +0200

    update message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello from test_branch");
+        System.out.println("Hello from 123");
     }
 }
